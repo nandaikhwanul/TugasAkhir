0bcd39e0a99c08b152c3c3128a544ea2192cf446
Nanda Ikhwanul <nanda.pertama465@gmail.com>
2025-08-12 03:57:58 +0700

    l

--- backend/controllers/Login.js
+++ backend/controllers/Login.js
@@ -21,7 +21,7 @@ try {
 // Helper untuk set cookie JWT
 function setTokenCookie(res, token) {
     res.cookie("token", token, {
-        httpOnly: true,
+        httpOnly: false,
         secure: true,
         sameSite: "none",
         maxAge: 30 * 60 * 1000 // 30 menit
