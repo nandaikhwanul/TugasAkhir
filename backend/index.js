f38fa2fe1e53cf2819ac66d3a00f741708a41517
Nanda Ikhwanul <nanda.pertama465@gmail.com>
2025-08-12 00:47:44 +0700

    menyetel cors

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.use(session({
 // Izinkan CORS untuk mengakses localhost:3000
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:3000']
+    origin: ['tugasakhir-production-0986.up.railway.app']
 }));
 
 app.use(express.json());
